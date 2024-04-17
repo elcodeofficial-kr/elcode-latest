16a5e7205fbe2be6733b81fd20de5de707905112
elcodeofficial-kr <admin@elcode.co.kr>
2024-04-17 16:51:48 +0900

    initialize shadcn/ui

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <h1 className="text-3xl text-cyan-500">Tailwind CSS Testing</h1>
-    </>
-  );
-}
-
-export default App;
---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/shared/main/ui/button";
+function App() {
+  return (
+    <>
+      <Button>Click me</Button>
+    </>
+  );
+}
+
+export default App;
